db537d6884eb4e0f2d52d9e83f71d37f98df9adb
hitrik <hitrick84@ya.ru>
2022-08-21 14:48:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-1z5hdj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-1z5hdj)
---
+++ index.ts
@@ -0,0 +1,79 @@
+// Паттерн Стратегия
+// определяет семейство алгоритмов, инкапсулирует их и обеспечивает их взаимозаменяемость
+
+// интерфейс поведения описывает движение персонажа
+interface IMovableBehavior {
+  move: (name: string) => void;
+  jump?: (name: string) => void;
+}
+
+// Классы поведения реализуют основной интерфейс поведения
+// каждый класс для своего вида перемещения
+class Movable implements IMovableBehavior {
+  move(name: string) {
+    console.log(name + ' can walk');
+  }
+
+  jump() {
+    console.log(name + ' can jump');
+  }
+}
+
+class Flyable implements IMovableBehavior {
+  move(name) {
+    console.log(name + ' can fly');
+  }
+}
+
+class Swimmable implements IMovableBehavior {
+  move(name) {
+    console.log(name + ' can swim');
+  }
+}
+
+//абстрактный класс от которого наследуются основные сущности
+abstract class Person {
+  movable: IMovableBehavior;
+  name: string;
+
+  setBehavior(m: IMovableBehavior) {
+    this.movable = m;
+  }
+}
+
+class Man extends Person {
+  constructor(name: string) {
+    super();
+    this.name = name;
+  }
+  movable = new Movable();
+}
+
+class Humanoid extends Person {
+  constructor(name: string) {
+    super();
+    this.name = name;
+  }
+  movable = new Flyable();
+}
+
+function init() {
+  const alien = new Humanoid('Чужой');
+  const man = new Man('Валера');
+
+  man.movable.move(man.name);
+  alien.movable.move(alien.name);
+
+  setTimeout(() => {
+    // когда начался потоп заставляем наши экземпляры плавать
+    console.log('The Great flood 2.0 had began');
+    const swimAbility = new Swimmable();
+    man.setBehavior(swimAbility);
+    alien.setBehavior(swimAbility);
+
+    man.movable.move(man.name);
+    alien.movable.move(man.name);
+  }, 3000);
+}
+
+init();
